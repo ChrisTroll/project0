50a62c934eea0892d4586b0fa9c8b818da30fbd1
ChrisTroll <ctroll333@gmail.com>
2019-10-17 10:05:53 -0400

    First commit

---
+++ src/main/java/Project0/BankManager/App.java
@@ -0,0 +1,13 @@
+package Project0.BankManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
